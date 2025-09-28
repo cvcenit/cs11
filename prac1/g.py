666a5160264b21fc04afa7bb91bf3a8ab845d058
Chicco <cvcenit@up.edu.ph>
2025-09-28 23:14:22 +0800

    comprehension and recursion

--- prac1/g.py
+++ prac1/g.py
@@ -2,8 +2,10 @@ from decorators import *
 
 @show_calls
 def suspense_sort(ratings):
-    if not ratings:
-        return ()
+    if is_sorted(ratings):
+        return ratings
+    else:
+        
 
 print(suspense_sort(()))
 print(suspense_sort((0, 0, 1, 1, 2, 2, 0, 0)))
