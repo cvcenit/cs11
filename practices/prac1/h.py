b4f8d145c4d7e61f86a41f7090beb23ef4eb981f
Chicco <cvcenit@up.edu.ph>
2025-10-19 17:00:40 +0800

    helllo

--- practices/prac1/h.py
+++ practices/prac1/h.py
@@ -1,5 +1,30 @@
-def transpose(matrix):
+def column_to_rows(matrix):
     if not matrix:
         return ()
     else:
-        return matrix[0][0], matrix[1][0]
+        return matrix[0][0], *column_to_rows(matrix[1:])
+
+def rest_columns(matrix):
+    if len(matrix) == 1:
+        return (matrix[0][1:],)
+    else:
+        return matrix[0][1:], *rest_columns(matrix[1:])
+
+def transpose(matrix):
+    if not matrix[0]:
+        return ()
+    else:
+        return column_to_rows(matrix), *transpose(rest_columns(matrix))
+
+print(transpose((
+    (1, 2, 3),
+    (4, 5, 6),
+    (7, 8, 9),
+    (8, 7, 6),
+))
+)
+
+print(transpose((
+    (1,),
+))
+)
